32223109872e6fc96f4c2ac6374ef382760040f1
root <Astesio Jose do Camor Junior>
2014-01-25 18:03:11 -0200

    aprendendo sobre exception e criar minhas proprias exceptions

--- java-orientado-a-objetos/src/TestaConta.java
+++ java-orientado-a-objetos/src/TestaConta.java
@@ -1,8 +1,16 @@
 public class TestaConta {
 	public static void main(String[] args) {
-		Conta conta = new Conta("Mauricio", 123, 1400.00);
-		conta.setNome("Mauricio");
-		conta.imprime();
-		System.out.println(conta.getNome());
+		Conta joao = new Conta();
+		
+		joao.deposita(50.0);
+		try {
+			joao.saca(300.0);			
+		} catch(SaldoInsuficienteException e) { 
+			System.out.println("Saldo está insuficiente: " + e.getSaldoAtual());
+		}
+		catch(Exception e ){
+			System.out.println("Um outro erro qualquer");
+		}
+		System.out.println(joao.getSaldo());
 	}
 }
