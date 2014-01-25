cb84533a9ef44bfcd7ec31add3e6e558d5a79b5b
root <Astesio Jose do Camor Junior>
2014-01-24 22:07:01 -0200

    aprendendo um pouco sobre oo

--- primeiros-passos-java/src/Conta.java
+++ primeiros-passos-java/src/Conta.java
@@ -1,67 +1,58 @@
-class Conta {
-	int numero;
-	double saldo;
-	int agencia;
-	Pessoa titular;
+public class Conta {
+	private int numero;
+	private double saldo;
+	private double limite;
+	private int agencia;
+	private static int totalDeContas = 0;
 	
-	void deposita(double valorASerDepositado){
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public void deposita(double valorASerDepositado){
 		this.saldo += valorASerDepositado;
 	}
 	
-	void saca(double valorASerSacado) {
-		if(this.saldo >= valorASerSacado) {
+	public void saca(double valorASerSacado) {
+		if(this.saldo < valorASerSacado + this.limite){
+			System.out.println("Valor insuficiente");	
+		}
+		else {
 			this.saldo -= valorASerSacado;
 		}
 	}
-	
-//	void transfere(double valor, Conta origem, Conta destino) {
-//		this.saldo -= valor;
-//		destino.saldo += valor;
-//	}
-	
-	
+
 	// mudança dificil de se perceber proquer tiramos a conta origem
-	void transfere(double valor, Conta destino) {
+	public void transfere(double valor, Conta destino) {
 		this.saldo -= valor;
 		destino.saldo += valor;
-	}
-}
-
-class Pessoa {
-	String nome;
-	String cpf;
-	String dataNascimento;
-	
-}
-
-class Programa {
-	public static void main(String[] args) {
-		Conta mauricio = new Conta();
-			mauricio.numero = 123;
-			mauricio.saldo = 800.0;
-			mauricio.agencia = 842;
-			
-			mauricio.titular = new Pessoa();
-			mauricio.titular.nome = "Mauricio Aniche";
-			mauricio.titular.cpf ="123456789";
-			mauricio.titular.dataNascimento = "09/05/1989";
-			
-			mauricio.deposita(1000);
-			mauricio.saca(1700.00);
-			
-			
-		Conta guilherme = new Conta();
-			guilherme.numero = 456;
-			guilherme.saldo = 1200.0;
-			
-			guilherme.deposita(800.0);
-			guilherme.saca(3000.0);
-			
-			mauricio.transfere(200, guilherme);
-			guilherme.transfere(400, mauricio);
-			
-			
-			System.out.println(mauricio.saldo);
-			System.out.println(guilherme.saldo);
 	}
 }
