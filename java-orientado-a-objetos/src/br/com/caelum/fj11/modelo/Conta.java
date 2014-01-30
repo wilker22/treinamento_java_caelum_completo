50987bd3549c4410aba3aa862189285561697f71
root <Astesio Jose do Camor Junior>
2014-01-30 21:03:11 -0200

    aprendendo um pouco sobre coleções, hascode, comparable, etc

--- java-orientado-a-objetos/src/br/com/caelum/fj11/modelo/Conta.java
+++ java-orientado-a-objetos/src/br/com/caelum/fj11/modelo/Conta.java
@@ -1,5 +1,5 @@
 package br.com.caelum.fj11.modelo;
-public class Conta {
+public class Conta implements Comparable<Conta> {
 	private double saldo;
 	private String nome;
 	private int agencia;
@@ -66,12 +66,36 @@ public class Conta {
 	}
 	
 	@Override
-	public boolean equals(Object obj) {
-		if(!(obj instanceof Conta)) return false;
-		
-		Conta outra = (Conta) obj;
-		return outra.saldo == this.saldo;
-//		if(outra.saldo == this.saldo) return true;
-//		else return false;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(saldo);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (Double.doubleToLongBits(saldo) != Double
+				.doubleToLongBits(other.saldo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Conta outraConta) {
+		if(this.saldo < outraConta.saldo) return -1;
+		if(this.saldo > outraConta.saldo) return 1;
+		return 0;
+	}
+	
+	
 }
